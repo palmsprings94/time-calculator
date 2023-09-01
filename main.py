82a54d1f6cf4cd3eac26ee6ba812094a8b535d51
Terence <143241259+palmsprings94@users.noreply.github.com>
2023-09-01 21:52:07 +0800

    Initial commit

--- main.py
+++ main.py
@@ -2,5 +2,9 @@
 from time_calculator import add_time
 
 
-print(add_time("11:06 PM", "2:02"))
-
+add_time("3:00 PM", "3:10")
+add_time("11:30 AM", "2:32", "Monday")
+add_time("11:43 AM", "00:20")
+add_time("10:10 PM", "3:30")
+add_time("11:43 PM", "24:20", "Tuesday")
+add_time("6:30 PM", "205:12")
